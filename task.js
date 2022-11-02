2746c011837d15079eb834308724839403908c74
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2022-11-02 21:51:07 +0300

    feat: making footer

---
+++ task.js
@@ -0,0 +1,7 @@
+function calculator() {
+    this.read() = function(){
+        this.a = +prompt('enter the first value', 0);
+        this.b = +prompt('enter the second value', 0);
+        };
+        return (this.a + this.b);
+    };

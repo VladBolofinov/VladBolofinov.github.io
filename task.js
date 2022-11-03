e223914a8351d3003b605d77aaf64ffd7180d0a8
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2022-11-03 23:14:59 +0300

    feat:done footer

--- task.js
+++
@@ -1,7 +0,0 @@
-function calculator() {
-    this.read() = function(){
-        this.a = +prompt('enter the first value', 0);
-        this.b = +prompt('enter the second value', 0);
-        };
-        return (this.a + this.b);
-    };

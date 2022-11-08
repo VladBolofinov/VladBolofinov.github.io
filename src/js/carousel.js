a2d3aa96895495c644c8843d8f1c0f97541408d1
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2022-11-08 16:38:38 +0300

    add some sheet

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,7 +1,17 @@
-let items = document.querySelectorAll('.card_animal');
-document.querySelector('.slider_btn_prev').addEventListener('click',function() {
-    
-});
+let leftCtrl = document.querySelector('.slider_btn.prev');
+let slides = document.querySelectorAll('.container_carousel');
+let cards = document.querySelectorAll('.container_carousel.active .card_animal');
+
+console.log(cards[6].classList.contains('show'));
+
+
+/* leftCtrl.addEventListener('click', () => {
+    slides[0].classList.remove('.active');
+    slides[0].classList.add('non-active');
+    slides[1].classList.remove('non-active');
+    slides[1].classList.add('active');
+}); */
+
 
 
 let placeInArray = function(){     //Функция, создаёт массив рандомных чисел(блоков), которые будут на странице прогружаться
@@ -21,10 +31,21 @@ let placeInArray = function(){     //Функция, создаёт массив
 
     return newArr;
 }
+let arrNumbers = placeInArray();
 
-console.log(placeInArray());
-
-
+function mixCarousel(){
+    for (let i = 0; i < 8; i++){
+        if (cards[i].classList.contains('hide')){
+            cards[i].classList.remove('.hide');
+            cards[i].classList.add('.show');
+            cards[i-1].classList.remove('.show');
+            cards[i-1].classList.add('.hide');
+        }
+        cards[i].innerHTML = cards[arrNumbers[i]].innerHTML;
+        cards[arrNumbers[i]].innerHTML = cards[i].innerHTML;
+    }
+}
+mixCarousel();
 
 
 
